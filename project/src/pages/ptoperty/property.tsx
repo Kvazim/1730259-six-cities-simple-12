3862d3abff85b9efdc9864763d5f11a180fb78f9
Kvazim <mesvladimir@gmail.com>
2023-03-13 21:02:33 +0300

    Страница отзывов

--- project/src/pages/ptoperty/property.tsx
+++ project/src/pages/ptoperty/property.tsx
@@ -1,9 +1,11 @@
 import { Helmet } from 'react-helmet-async';
 import {Link, useParams} from 'react-router-dom';
 import Header from '../../components/header/header';
-// import PropertyPhoto from '../../components/property-photo/property-photo';
+import PropertyInside from '../../components/property-inside/property-inside';
+import PropertyPhoto from '../../components/property-photo/property-photo';
 import { Offers } from '../../types/cards';
 import { ReviewsList } from '../../types/reviews';
+import PageNotFound from '../page-not-found/page-not-found';
 
 type PropertyProps = {
   offers: Offers;
@@ -11,7 +13,14 @@ type PropertyProps = {
 }
 
 function Property({offers, reviews}:PropertyProps): JSX.Element {
-  // const idRoom = useParams();
+  const {id} = useParams();
+  const property = offers.find((offer) => String(offer.id) === String(id));
+
+  if (property === undefined) {
+    return <PageNotFound />;
+  }
+
+  const {images, isPremium, rating, type, bedrooms, maxAdults, price, goods, host} = property;
   return (
     <div className="page">
       <Helmet>
@@ -22,19 +31,25 @@ function Property({offers, reviews}:PropertyProps): JSX.Element {
       <main className="page__main page__main--property">
         <section className="property">
           <div className="property__gallery-container container">
-            {/* <div className="property__gallery">
+            <div className="property__gallery">
               {
                 images.map((photoUrl, index) => (
                   <PropertyPhoto key={String(photoUrl) + String(index)} photoUrl={photoUrl} />
                 ))
               }
-            </div> */}
+            </div>
           </div>
           <div className="property__container container">
             <div className="property__wrapper">
-              <div className="property__mark">
-                <span>Premium</span>
-              </div>
+              {
+                isPremium
+                  ?
+                  <div className="property__mark">
+                    <span>Premium</span>
+                  </div>
+                  :
+                  null
+              }
               <div className="property__name-wrapper">
                 <h1 className="property__name">
                   Beautiful &amp; luxurious studio at great location
@@ -45,70 +60,52 @@ function Property({offers, reviews}:PropertyProps): JSX.Element {
                   <span style={{ width: '80%' }}></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
-                <span className="property__rating-value rating__value">4.8</span>
+                <span className="property__rating-value rating__value">{rating}</span>
               </div>
               <ul className="property__features">
                 <li className="property__feature property__feature--entire">
-                  Apartment
+                  {type}
                 </li>
                 <li className="property__feature property__feature--bedrooms">
-                  3 Bedrooms
+                  {bedrooms} Bedrooms
                 </li>
                 <li className="property__feature property__feature--adults">
-                  Max 4 adults
+                  Max {maxAdults} adults
                 </li>
               </ul>
               <div className="property__price">
-                <b className="property__price-value">&euro;120</b>
+                <b className="property__price-value">&euro;{price}</b>
                 <span className="property__price-text">&nbsp;night</span>
               </div>
               <div className="property__inside">
                 <h2 className="property__inside-title">What&apos;s inside</h2>
                 <ul className="property__inside-list">
-                  <li className="property__inside-item">
-                    Wi-Fi
-                  </li>
-                  <li className="property__inside-item">
-                    Washing machine
-                  </li>
-                  <li className="property__inside-item">
-                    Towels
-                  </li>
-                  <li className="property__inside-item">
-                    Heating
-                  </li>
-                  <li className="property__inside-item">
-                    Coffee machine
-                  </li>
-                  <li className="property__inside-item">
-                    Baby seat
-                  </li>
-                  <li className="property__inside-item">
-                    Kitchen
-                  </li>
-                  <li className="property__inside-item">
-                    Dishwasher
-                  </li>
-                  <li className="property__inside-item">
-                    Cabel TV
-                  </li>
-                  <li className="property__inside-item">
-                    Fridge
-                  </li>
+                  {
+                    Array.isArray(goods) && goods.length > 0 &&
+                      goods.map((good, index) => (
+                        <PropertyInside key={String(index) + String(good)} good={good} />
+                      ))
+                  }
                 </ul>
               </div>
               <div className="property__host">
                 <h2 className="property__host-title">Meet the host</h2>
                 <div className="property__host-user user">
-                  <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar" />
+                  <div className={`property__avatar-wrapper ${host.isPro ? 'property__avatar-wrapper--pro' : ''} user__avatar-wrapper`} >
+                    <img className="property__avatar user__avatar" src={host.avatarUrl} width="74" height="74" alt="Host avatar" />
                   </div>
                   <span className="property__user-name">
-                    Angelina
+                    {host.name}
                   </span>
-                  <span className="property__user-status">
-                    Pro
-                  </span>
+                  {
+                    host.isPro
+                      ?
+                      <span className="property__user-status">
+                        Pro
+                      </span>
+                      :
+                      null
+                  }
                 </div>
                 <div className="property__description">
                   <p className="property__text">
