09caf73307ddf604a831e0c90911426d607905c2
Vladimir <mesvladimir@yandex.ru>
2023-03-17 12:22:19 +0300

    Состояние Звезд

--- project/src/components/rating-stars/rating-stars.tsx
+++ project/src/components/rating-stars/rating-stars.tsx
@@ -1,28 +1,24 @@
-import { Fragment, useState } from 'react';
+import { Fragment, ChangeEvent } from 'react';
 
 type RatingStarsProps = {
   stars: string[];
+  isChecked: string;
+  onChangeChecked: ({ target }: ChangeEvent<HTMLInputElement>) => void;
 }
 
-function RatingStars({ stars }: RatingStarsProps): JSX.Element {
-  const [isChecked, setIsChecked] = useState({
-    rating: '0',
-    comment: ''
-  });
-  const onClickInput = (evt: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-    const { value } = evt.target;
-    setIsChecked({ ...isChecked, rating: value });
-  };
+function RatingStars({ stars, onChangeChecked, isChecked }: RatingStarsProps): JSX.Element {
   return (
-    <div className="reviews__rating-form form__rating">
+    <div className="reviews__rating-form form__rating" onChange={onChangeChecked}>
       {
         stars && stars.length > 0 && stars.map((title, index) => (
           <Fragment key={title}>
             <input
               className="form__rating-input visually-hidden"
-              name="rating" value={`${stars.length - index}`}
-              id={`${stars.length - 1}-stars`} type="radio"
-              onClick={onClickInput}
+              name="rating"
+              value={`${stars.length - index}`}
+              id={`${stars.length - index}-stars`}
+              type="radio"
+              checked={isChecked === `${stars.length - index}`}
             />
             <label htmlFor={`${stars.length - index}-stars`} className="reviews__rating-label form__rating-label" title={title}>
               <svg className="form__star-image" width="37" height="33">
