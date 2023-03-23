fae1027eeb9e28f350bed38bcf496b958c7cb3f9
mesvladimir <mesvladimir@yandex.ru>
2023-03-23 14:47:31 +0300

    перенос основной разметки в мэйн

--- project/src/components/cities/cities.tsx
+++ project/src/components/cities/cities.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import CitiesCard from '../cities-card/cities-card';
 import { Offer, Offers } from '../../types/cards';
-import Tabs from '../tabs/tabs';
 import PlacesSorting from '../places-sorting/places-sorting';
 import Map from '../map/map';
 
@@ -13,29 +12,23 @@ function Cities({ offers }: CitiesProp): JSX.Element {
   const [focusCard, setFocusCard] = useState<Offer | null>(null);
 
   return (
-    <main className="page__main page__main--index">
-      <h1 className="visually-hidden">Cities</h1>
-      <Tabs />
-      <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{offers.length} places to stay in Amsterdam</b>
-            <PlacesSorting />
-            <div className="cities__places-list places__list tabs__content">
-              {
-                offers.map((offer) => (
-                  <CitiesCard key={offer.id} className={'cities'} offer={offer} onFocusCard={setFocusCard} />
-                ))
-              }
-            </div>
-          </section>
-          <div className="cities__right-section">
-            <Map className={'cities'} offers={offers} focusCard={focusCard} />
-          </div>
+    <div className="cities__places-container container">
+      <section className="cities__places places">
+        <h2 className="visually-hidden">Places</h2>
+        <b className="places__found">{offers.length} places to stay in Amsterdam</b>
+        <PlacesSorting />
+        <div className="cities__places-list places__list tabs__content">
+          {
+            offers.map((offer) => (
+              <CitiesCard key={offer.id} className={'cities'} offer={offer} onFocusCard={setFocusCard} />
+            ))
+          }
         </div>
+      </section>
+      <div className="cities__right-section">
+        <Map className={'cities'} offers={offers} focusCard={focusCard} />
       </div>
-    </main>
+    </div>
   );
 }
 
--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import Cities from '../../components/cities/cities';
+import Tabs from '../../components/tabs/tabs';
 import { Offers } from '../../types/cards';
 import CitiesEmpty from '../cities-empty/cities-empty';
 
@@ -9,19 +10,22 @@ type MainScreenProps = {
 
 function Main({ offers }: MainScreenProps): JSX.Element {
   return (
-    <>
+    <main className={`page__main page__main--index ${offers.length < 1 ? 'page__main--index-empty' : '' }`}>
       <Helmet>
         <title>six cities simple</title>
       </Helmet>
-
-      {
-        offers && offers.length > 0
-          ?
-          <Cities offers={offers} />
-          :
-          <CitiesEmpty />
-      }
-    </>
+      <h1 className="visually-hidden">Cities</h1>
+      <Tabs />
+      <div className="cities">
+        {
+          offers && offers.length > 0
+            ?
+            <Cities offers={offers} />
+            :
+            <CitiesEmpty />
+        }
+      </div>
+    </main>
   );
 }
 
