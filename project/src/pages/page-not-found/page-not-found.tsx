3430181d8527131322a43a2980a790f0d99fcc5e
Kvazim <mesvladimir@gmail.com>
2023-03-27 10:15:20 +0300

    Сделал страницу 404

--- project/src/pages/page-not-found/page-not-found.tsx
+++ project/src/pages/page-not-found/page-not-found.tsx
@@ -1,19 +1,14 @@
 import { Link } from 'react-router-dom';
-import Header from '../../components/header/header';
 import './style.css';
 
 function PageNotFound(): JSX.Element {
   return (
-    <div className="page">
-      <Header />
-
-      <main className="page__main page__main--page-not-found">
-        <section className="page-not-found">
-          <h1 className="page-not-found__title">404 Page not found</h1>
-          <Link className="page-not-found__link" to="/"><span>Вернутся на главную страницу</span></Link>
-        </section>
-      </main>
-    </div>
+    <main className="page__main page__main--page-not-found">
+      <section className="page-not-found">
+        <h1 className="page-not-found__title">404 Page not found</h1>
+        <Link className="page-not-found__link" to="/"><span>Вернутся на главную страницу</span></Link>
+      </section>
+    </main>
   );
 }
 
